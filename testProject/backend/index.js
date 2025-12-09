c4e932012030ee347cc0f3d63ce7bab870097ae1
Janmenjay <janmejaypanigrahi22@gmail.com>
2025-12-09 14:13:39 +0530

    Feature:add CI pipeline

---
+++ testProject/backend/index.js
@@ -0,0 +1,5 @@
+console.log('Hello from simple Node app!');
+setTimeout(()=>{
+    console.log('This is a delayed message after 2 seconds.');
+},2000);
+console.log('Goodbye from simple Node app!');
